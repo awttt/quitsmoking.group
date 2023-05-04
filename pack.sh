3d3f5c2ced5e8dae7fc85c39dbaa99ffa042d389
went.an <went.an@hnair.com>
2023-05-05 00:40:42 +0800

    update

--- pack.sh
+++ pack.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-# 请在此处设置您的实际值
-LOCAL_PROJECT_PATH="build"
-REMOTE_USER="ubuntu"
-REMOTE_HOST="106.53.64.77"
-REMOTE_PATH="/home/${REMOTE_USER}/docusaurus_project"
-CONTAINER_NAME="focused_panini"
-CONTAINER_DEST_PATH="/app"
+#!/bin/bash
 
-# 使用 scp 将本地 Docusaurus 项目复制到远程服务器
-scp -r ${LOCAL_PROJECT_PATH} ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
+# 服务器信息
+SSH_HOST="106.53.64.77"
+SSH_PORT="22"
+SSH_USER="ubuntu"
+SSH_PASSWORD="zxcxz111."
+REMOTE_DIR="/var/www/html"
 
-# 在远程服务器上，将文件从服务器文件系统复制到 Docker 容器中
-ssh ${REMOTE_USER}@${REMOTE_HOST} "docker cp ${REMOTE_PATH} ${CONTAINER_NAME}:${CONTAINER_DEST_PATH}"
+# 进入项目根目录，执行 pnpm build
+# cd /path/to/your/project
+pnpm build
 
-# 在远程服务器上，启动容器（如果尚未启动）
-ssh ${REMOTE_USER}@${REMOTE_HOST} "docker start ${CONTAINER_NAME}"
+# 将 build 文件夹上传到云服务器
+echo "开始执行脚本：$(date) 将 build 文件夹上传到云服务器" 
+sshpass -p "$SSH_PASSWORD" scp -r -P $SSH_PORT ./build $SSH_USER@$SSH_HOST:/tmp
 
-# 在远程服务器上，进入容器并运行 Docusaurus 项目构建命令
-ssh ${REMOTE_USER}@${REMOTE_HOST} "docker exec ${CONTAINER_NAME} /bin/bash -c 'cd ${CONTAINER_DEST_PATH} && npm init && npm install && npm run build && npm install -g serve'"
+# 清空云服务器的 /var/www/html/ 文件夹
+echo "正在清空历史数据..." 
+sshpass -p "$SSH_PASSWORD" ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "sudo rm -rf $REMOTE_DIR/*"
 
-# 在远程服务器上，重新启动容器以运行 Docusaurus 项目
-ssh ${REMOTE_USER}@${REMOTE_HOST} "docker restart ${CONTAINER_NAME}"
+# 将 build 文件夹里的所有文件复制到 /var/www/html/ 目录中
+echo "正在更新文件..." 
+sshpass -p "$SSH_PASSWORD" ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "sudo cp -r /tmp/build/* $REMOTE_DIR/"
+
+# 删除云服务器上的临时文件夹
+echo "正在删除临时文件..." 
+sshpass -p "$SSH_PASSWORD" ssh -p $SSH_PORT $SSH_USER@$SSH_HOST "sudo rm -rf /tmp/build"
+
+echo "$(date) 脚本执行完成，更新成功!!!" 
 
